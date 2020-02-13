fdabdd21dd97c8b30dadce17f1cc57151ebdff6b
Devil7DK <dineshthangavel47@gmail.com>
2020-02-13 13:38:57 +0530

    Add UrlBuilder class

---
+++ Jira Bulk Issues Helper/Utils/UrlBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace Jira_Bulk_Issues_Helper.Utils
+{
+    public class UrlBuilder
+    {
+        #region Variables
+        private string domain = "";
+
+        private string baseUrl = "https://{0}.atlassian.net/rest/api/2";
+        #endregion
+
+        #region Constructor
+        public UrlBuilder(string domain)
+        {
+            this.domain = domain;
+        }
+        #endregion
+
+        #region Public Functions
+        public string getBaseUrl() => string.Format(this.baseUrl, domain);
+
+        public string getMetaUrl() => "/issue/createmeta";
+        #endregion
+    }
+}
