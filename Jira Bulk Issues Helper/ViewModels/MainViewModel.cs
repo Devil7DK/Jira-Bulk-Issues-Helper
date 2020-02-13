f5da222bc6d2106a999405c60df194ad94ae03dc
Devil7DK <dineshthangavel47@gmail.com>
2020-02-13 12:56:51 +0530

    Design basic UI with bindings

--- Jira Bulk Issues Helper/ViewModels/MainViewModel.cs
+++ Jira Bulk Issues Helper/ViewModels/MainViewModel.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 using DevExpress.Mvvm;
 
 namespace Jira_Bulk_Issues_Helper.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Variables
+        private string email = "";
+        private string apiToken = "";
+        private string domainName = "";
+        #endregion
+
+        #region Properties
+        public string EMail { get => email; set => SetProperty(ref email, value, "EMail"); }
+        public string APIToken { get => apiToken; set => SetProperty(ref apiToken, value,"APIToken"); }
+        public string DomainName { get => domainName; set => SetProperty(ref domainName, value, "DomainName"); }
+        #endregion
+
+        #region Commands
+        public AsyncCommand Login { get; }
+        #endregion
+
+        #region Command Implementation
+        private Task login ()
+        {
+            return Task.Run(() =>
+            {
+                /// TODO
+            });
+        }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new AsyncCommand(login);
+        }
+        #endregion
     }
 }
